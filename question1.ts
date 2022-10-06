63334482ec0f8509c89b52aff24f2d9f594caf8b
Bishen Patel <patelzz007@gmail.com>
2022-10-06 23:40:37 +0800

    Question 1.

---
+++ question1.ts
@@ -0,0 +1,7 @@
+let is_array = (input) => {
+    if (toString.call(input) === "[object Array]") return true;
+    return false;
+  };
+  
+  console.log(is_array("w3resource"));
+  console.log(is_array([1, 2, 4, 0]));
