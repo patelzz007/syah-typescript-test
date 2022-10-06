7542ef96e8ce08afa4e9b335ab5befd00fbf22f2
Bishen Patel <patelzz007@gmail.com>
2022-10-06 23:58:28 +0800

    Question 6.

---
+++ question6.js
@@ -0,0 +1,3 @@
+var numericArray = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
+var sortedArray = numericArray.sort(function (n1, n2) { return n1 - n2; });
+console.log(sortedArray);
---
+++ question6.ts
@@ -0,0 +1,5 @@
+var numericArray: number[] = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+
+var sortedArray: number[] = numericArray.sort((n1,n2) => n1 - n2);
+
+console.log(sortedArray);
