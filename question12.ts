bc57d28956b88fcdf1c8ca8ad86ad062c9fcca7a
Bishen Patel <patelzz007@gmail.com>
2022-10-07 00:35:51 +0800

    Question 12.

---
+++ question12.ts
@@ -0,0 +1,21 @@
+// diff between just two arrays:
+function arrayDiff(a, b) {
+  var arrays = Array.prototype.slice.call(arguments);
+  var diff: string[] = [];
+
+  arrays.forEach(function (arr, i) {
+    var other = i === 1 ? a : b;
+    arr.forEach(function (x) {
+      if (other.indexOf(x) === -1) {
+        diff.push(x);
+      }
+    });
+  });
+
+  return diff;
+}
+
+const a123 = [1, 2, 3];
+const b123 = [100, 2, 1, 10];
+
+console.log(arrayDiff(a123, b123));
