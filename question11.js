64e97ea8b458c858a65434b03f314d1a10d3e0f2
Bishen Patel <patelzz007@gmail.com>
2022-10-07 00:19:26 +0800

    Question 11.

--- question11.js
+++ question11.js
@@ -1,7 +1,3 @@
-// let a = [1, 2, 3];
-// let b = [100, 2, 1, 10];
-// let union = [...new Set([...a, ...b])];
-// console.log(union);
 var a = [1, 2, 3];
 var b = [100, 2, 1, 10];
 var c = a.concat(b).sort();
